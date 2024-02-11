fdf635168d3301228e4810380e05fe2ce9f5b9cd
Maston <22679886+mastondzn@users.noreply.github.com>
2024-02-11 22:48:11 +0200

    feat: add `@mastondzn/eslint`

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "@mastondzn/eslint";
+
+// if you wish to see what this config adds
+// you can run `pnpx eslint-flat-config-viewer`
+export default defineConfig({
+  stylistic: false,
+
+  typescript: {
+    tsconfigPath: ["./tsconfig.json"],
+  },
+});
