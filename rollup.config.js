e2937a0b84e9833bb4873d39b74f1db2fb981dc3
Maston <22679886+mastondzn@users.noreply.github.com>
2024-02-24 04:58:29 +0200

    style: `lib` -> `src`

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
 
 export default defineConfig({
-  input: "lib/index.ts",
+  input: "src/index.ts",
   output: [
     { file: "dist/index.cjs", sourcemap: true, format: "cjs" },
     { file: "dist/index.js", sourcemap: true, format: "esm" },
--- vitest.config.js
+++ vitest.config.js
@@ -4,10 +4,10 @@ export default defineConfig({
   test: {
     exclude: [
       ...defaultExclude,
-      "./lib/utils/helpers.spec.ts",
-      "./lib/utils/setup.spec.ts",
+      "./src/utils/helpers.spec.ts",
+      "./src/utils/setup.spec.ts",
     ],
-    setupFiles: ["./lib/utils/setup.spec.ts"],
+    setupFiles: ["./src/utils/setup.spec.ts"],
     coverage: {
       reporter: ["text-summary", "json", "json-summary"],
       reportOnFailure: true,
