07da08feca5eaf05477a2f534a6a712a298f3696
Maston <22679886+mastondzn@users.noreply.github.com>
2024-03-03 23:59:01 +0200

    docs: add docs website (#1)
    This adds a new docs website with generated API reference that uses:
     - typedoc
     - typedoc-plugin-markdown
     - vitepress
    
    They get built and deployed on GitHub Pages using on every push on the
    master branch.

---
+++ .vitepress/config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "vitepress";
+
+import sidebar from "./typedoc/typedoc-sidebar.json";
+
+export default defineConfig({
+  lang: "en-US",
+  title: "dank-twitch-irc",
+  description: "A Twitch IRC client for Node.js",
+  srcDir: ".vitepress/typedoc",
+  base: process.env.GITHUB_ACTIONS ? "/dank-twitch-irc/" : "/",
+  cleanUrls: true,
+  themeConfig: {
+    nav: [
+      { link: "https://github.com/mastondzn/dank-twitch-irc", text: "GitHub" },
+    ],
+    sidebar: [
+      {
+        text: "API Reference",
+        link: "./globals",
+        items: sidebar
+          .map((item) => ({
+            ...item,
+            items: item.items.map((subItem) => ({
+              ...subItem,
+              link: subItem.link.replace(/^\/.vitepress\/typedoc/, ""),
+            })),
+          }))
+          .sort((a, b) => a.text.localeCompare(b.text)),
+      },
+    ],
+  },
+});
