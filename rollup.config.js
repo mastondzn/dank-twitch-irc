ba4bdf239dc2dc1284aba2de2636038593ea092c
Maston <22679886+mastondzn@users.noreply.github.com>
2024-03-05 01:52:40 +0200

    build: use rollup's `preserveModules` option
    This makes it possible for a package user to dig into the dist folder for a certain export (like `parseTwitchMessage`) if they cant import it otherwise (not exported or non-node environments).
    
    Also allows possibility of further expanding of the `exports` fields in package.json, and makes declaration files match one for one.

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,24 @@
 import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
-
-/** @type {import("typescript-transform-paths").default} */
-const paths = (await import("typescript-transform-paths")).default.default;
-// "typescript-transform-paths" has a broken default export
+import paths from "typescript-transform-paths";
 
 export default defineConfig({
   input: "src/index.ts",
   output: [
-    { file: "dist/index.cjs", sourcemap: true, format: "cjs" },
-    { file: "dist/index.js", sourcemap: true, format: "esm" },
-  ],
+    { format: "esm", entryFileNames: "[name].js" },
+    { format: "cjs", entryFileNames: "[name].cjs" },
+  ].map((output) => ({
+    sourcemap: true,
+    dir: "dist",
+    preserveModules: true,
+    ...output,
+  })),
   plugins: [
     typescript({
       exclude: ["./tests/**"],
       transformers: {
-        before: [{ type: "program", factory: paths }],
-        afterDeclarations: [{ type: "program", factory: paths }],
+        before: [{ type: "program", factory: paths.default }],
+        afterDeclarations: [{ type: "program", factory: paths.default }],
       },
     }),
   ],
